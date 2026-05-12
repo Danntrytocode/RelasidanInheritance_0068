48194711a6ed5d5c80b4973ecbc05eadda31f740
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-05-12 17:00:25 +0700

    add class anak with member nama

--- Agregasi/anak.h
+++ Agregasi/anak.h
@@ -3,3 +3,11 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+//create class anak di file header anak.h
+class anak
+{
+public:
+    //deklarasi member variabel pada class anak
+    string nama;
+}
