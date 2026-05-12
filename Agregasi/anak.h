ab15a05ad25eb25619fab0574f85d30b2cbd2d10
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-05-12 17:02:37 +0700

    add constructor and destructor in class

--- Agregasi/anak.h
+++ Agregasi/anak.h
@@ -10,4 +10,13 @@ class anak
 public:
     //deklarasi member variabel pada class anak
     string nama;
-}
+    //create constructor dan destructor pada class anak
+    anak(string pnama) : nama(pnama){
+        cout << " Anak \"" << nama << "\" ada \n" ;
+    }
+
+    ~anak(){
+        cout << "Anak \"" << nama << "\" tidak ada\n";
+    }
+};
+#endif
