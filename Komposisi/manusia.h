911292b108037b8098920207b010bd489e25b681
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-05-12 18:44:43 +0700

    add constructor and destructor in class manusia

--- Komposisi/manusia.h
+++ Komposisi/manusia.h
@@ -13,5 +13,13 @@ public:
     string name;
     jantung varJantung;
 
+    manusia(string pName)
+        : name(pName) {
+        cout << name << " hidup\n";
+    }
+
+    ~manusia() {
+        cout << name << " mati\n";
+    }
 };
 #endif 
