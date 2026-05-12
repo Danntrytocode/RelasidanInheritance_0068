abd9f9fb5136b09b036dc61978a11e1c03657f19
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-05-12 17:08:06 +0700

    pendefinisian 2 prosedur outside class

--- Agregasi/ibu.h
+++ Agregasi/ibu.h
@@ -25,4 +25,24 @@ public:
     void cetakAnak();
 };
 
+/*
+class anak {
+public:
+    string nama;
+    anak(string pnama) : nama(pnama) {}
+};*/
+//pendefinisian prosedur - prosedur pada class ibu di luar class
+void ibu::tambahAnak(anak* panak) {
+    daftar_anak.push_back(panak);
+}
+void ibu::cetakAnak(){
+    cout << "Daftar Anak dari Ibu \"" << this->nama << "\":\n";
+    /*for (auto& a : daftar_anak){
+        cout << a->nama << "\n";
+    }*/
+    for (int i = 0; i < daftar_anak.size(); i++){
+        cout << daftar_anak[i]-> nama << endl;
+    }
+    cout << endl;
+}
 #endif
