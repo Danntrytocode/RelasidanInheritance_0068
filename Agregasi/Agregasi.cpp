edde06bc31eb4f6c70d760b133e178ab86a176df
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-05-12 17:10:53 +0700

    pemberian nilai pointer tuk obyek dari class ibu dan anak

--- Agregasi/Agregasi.cpp
+++ Agregasi/Agregasi.cpp
@@ -6,3 +6,12 @@ using namespace std;
 //penyertaan file ibu.h dan anak.h
 #include "anak.h"
 #include "ibu.h"
+
+int main()
+{
+    ibu* varIbu = new ibu("syifa");
+    ibu* varIbu2 = new ibu("Nvidia");
+    anak* varAnak1 = new anak("Vermillion");
+    anak* varAnak2 = new anak("RTX 3090");
+    anak* varAnak3 = new anak("RTX 5060");
+}
