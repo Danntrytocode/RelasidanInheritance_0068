3baa20cbe5b6845f484c6807735be62b45f1f65a
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-05-12 20:12:28 +0700

    membuat class induk/mamah

--- TugasRelasidanInheritance.cpp
+++ TugasRelasidanInheritance.cpp
@@ -4,3 +4,26 @@
 
 using namespace std;
 
+//Class Induk
+class User {
+protected:
+    static int globalId; // Static member untuk auto-increment ID
+    int id;
+    string nama;
+    string email;
+
+public:
+    User(string pNama, string pEmail) : nama(pNama), email(pEmail) {
+        id = generateId();
+    }
+
+    //member function untuk generate ID otomatis
+    int generateId() {
+        return ++globalId;
+    }
+
+    //pengambil untuk kebutuhan display
+    int getId() { return id; }
+    string getNama() { return nama; }
+};
+
